c2efb32e3e486caa56715a56f40e880010e351b2
David Njoku <DnjFinance@outlook.fr>
2025-12-05 00:37:51 +0100

    SSH_to_instance_setup.sh

--- SSH_to_instance_setup.sh
+++ SSH_to_instance_setup.sh
@@ -1,4 +1,4 @@
-# Objectif atteind se connecter sur notre instance Ec2 par SSH avec une VM Ubuntu se trouvant en dehors de notre VPC avec une clef RSA.
+# Objectif atteint se connecter sur notre instance Ec2 par SSH avec une VM Ubuntu se trouvant en dehors de notre VPC avec une clef RSA.
 
 # VM Ubuntu dans VirtualBox(mode Bridge)
 # Instance ip publique : 51.44.168.103
